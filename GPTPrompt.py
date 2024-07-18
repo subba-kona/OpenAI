4f932942a52550f7b36dc39258a2eb34d73ef5cb
Subba Kona <subba.kona@gmail.com>
2024-07-18 10:22:33 +0530

    irctc

--- GPTPrompt.py
+++ GPTPrompt.py
@@ -1,13 +1,13 @@
 import openai
 
-openai.api_key = "<replace with your openAI secret>"
+openai.api_key = "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"
 
 def basicgeneration(prompt) :
      completion = openai.ChatCompletion.create( model="gpt-3.5-turbo", messages= [ {"role": "user", "content": prompt }])
 
      return completion.choices[0].message.content
 
-prompt= "please provide the details about Kakinada Anchorage Port"
+prompt= "please provide the details about Kakinada Parliament Constituency"
 
 print(basicgeneration(prompt))
 
